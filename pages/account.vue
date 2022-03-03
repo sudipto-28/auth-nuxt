748e4fb8512a40cc4bf9b4313ca6bac34916ab07
Sudipto Mondal <sudipto@genesysoftwares.com>
2022-03-03 14:02:22 +0600

    Initialization

---
+++ pages/account.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="">
+  <h1>Account</h1>
+  <button @click="logout">Logout</button>
+</div>
+
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+  methods: {
+    async logout() {
+      console.log('Logout')
+      await this.$auth.logout()
+      console.log('this.$auth', this.$auth)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
